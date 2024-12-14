d40ad555a102774372e00b8e8bfc3d9985ffb490
houren Antony <129468691+NkAntony777@users.noreply.github.com>
2024-12-14 18:54:39 +0800

    Update web.py

--- web.py
+++ web.py
@@ -11,9 +11,9 @@ transformer = TransformerNet().to(device)
 
 # Predefined models
 STYLE_MODELS = {
-    "Cuphead": "/models/cuphead_10000.pth",
-    "Starry Night": "/models/starry_night_10000.pth",
-    "Mosaic": "/models/mosaic_10000.pth"
+    "Cuphead": "cuphead_10000.pth",
+    "Starry Night": "starry_night_10000.pth",
+    "Mosaic": "mosaic_10000.pth"
 }
 
 def load_model(model_path):
