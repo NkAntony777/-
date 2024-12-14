e2daee315c6d4ee4fbd5b879f06858e8a92518b6
houren Antony <129468691+NkAntony777@users.noreply.github.com>
2024-12-14 20:50:03 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,21 +2,27 @@ from torchvision import transforms
 import torch
 import numpy as np
 import av
+import cv2
 
 # Mean and standard deviation used for pre-trained PyTorch models
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
 
-
 def extract_frames(video_path):
     """ Extracts only video frames from the input video """
-    
     video = av.open(video_path)
     video_stream = next(s for s in video.streams if s.type == 'video')  # Select video stream
     for frame in video.decode(video_stream):
         yield frame.to_image()
 
+def save_video(frames, output_path, fps, frame_size):
+    """ Saves a list of frames as a video file """
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    writer = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
 
+    for frame in frames:
+        writer.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    writer.release()
 
 def gram_matrix(y):
     """ Returns the gram matrix of y (used to compute style loss) """
@@ -25,7 +31,6 @@ def gram_matrix(y):
     features_t = features.transpose(1, 2)
     gram = features.bmm(features_t) / (c * h * w)
     return gram
-
 
 def train_transform(image_size):
     """ Transforms for training images """
@@ -39,20 +44,17 @@ def train_transform(image_size):
     )
     return transform
 
-
 def style_transform(image_size=None):
-    """ Transforms for style image """
+    """ Transforms for style image or video frames """
     resize = [transforms.Resize(image_size)] if image_size else []
     transform = transforms.Compose(resize + [transforms.ToTensor(), transforms.Normalize(mean, std)])
     return transform
-
 
 def denormalize(tensors):
     """ Denormalizes image tensors using mean and std """
     for c in range(3):
         tensors[:, c].mul_(std[c]).add_(mean[c])
     return tensors
-
 
 def deprocess(image_tensor):
     """ Denormalizes and rescales image tensor """
